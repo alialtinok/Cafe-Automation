5ea80c6e733545c8e1518a0abcc48c6321ade490
alialtinok <info.alialtinok@gmail.com>
2021-05-10 21:51:01 +0300

    first commit

---
+++ KafeOtomasyonu/KafeOtomasyonu/Controllers/IcecekController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KafeOtomasyonu.Models.Entitiy;
+
+
+namespace KafeOtomasyonu.Controllers
+{
+    public class IcecekController : Controller
+    {
+        CafeDBEntities aa = new CafeDBEntities();
+        
+        // GET: Icecek
+        public ActionResult Index()
+        {
+            var degerler = aa.Icecek.ToList();
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult IcecekEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult IcecekEkle(Icecek icecek)
+        {
+            aa.Icecek.Add(icecek);
+            aa.SaveChanges();
+            return RedirectToAction("Index","Icecek");
+            
+        }
+
+    }
+}
---
+++ KafeOtomasyonu/KafeOtomasyonu/Controllers/YiyecekController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KafeOtomasyonu.Models.Entitiy;
+
+
+namespace KafeOtomasyonu.Controllers
+{
+    public class YiyecekController : Controller
+    {
+        CafeDBEntities altinokCafe = new CafeDBEntities();
+
+        // GET: Yiyecek
+        public ActionResult Index()
+        {
+            var degerler = altinokCafe.Yiyecek.ToList();
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult YiyecekEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult YiyecekEkle(Yiyecek icecek)
+        {
+            altinokCafe.Yiyecek.Add(icecek);
+            altinokCafe.SaveChanges();
+            return RedirectToAction("Index", "Yiyecek");
+
+        }
+
+    }
+}
