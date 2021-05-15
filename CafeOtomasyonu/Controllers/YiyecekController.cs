a66791c6e1cb566a0000808ce0da10acfda7762b
alialtinok <info.alialtinok@gmail.com>
2021-05-15 19:55:07 +0300

    Food and Table functions
    Food and Tableware section functions added

--- CafeOtomasyonu/Controllers/YiyecekController.cs
+++ CafeOtomasyonu/Controllers/YiyecekController.cs
@@ -7,44 +7,45 @@ using System.Web.Mvc;
 
 namespace CafeOtomasyonu.Controllers
 {
-    public class MasaController : Controller
+    public class YiyecekController : Controller
     {
         CafeDBEntities db = new CafeDBEntities();
-        // GET: Masa
+
+
+        // GET: Yiyecek
         public ActionResult Index()
         {
-            var model = db.Masa.ToList();
+            var model = db.Yiyecek.ToList();
             return View(model);
         }
-
         [HttpGet]
         public ActionResult Kaydet()
         {
 
-            return View("MasaForm");
+            return View("YiyecekForm");
         }
 
         [HttpPost]
 
-        public ActionResult Kaydet(Masa masa)
+        public ActionResult Kaydet(Yiyecek yiyecek)
         {
-            if (masa.MasaID== 0)
+            if (yiyecek.YiyecekID == 0)
             {
-                db.Masa.Add(masa);
+                db.Yiyecek.Add(yiyecek);
             }
             else
             {
-                var guncellenecekMasa = db.Masa.Find(masa.MasaID);
-                if (guncellenecekMasa == null)
+                var guncellenecekyiyecek = db.Yiyecek.Find(yiyecek.YiyecekID);
+                if (guncellenecekyiyecek == null)
                 {
                     return HttpNotFound();
                 }
-                guncellenecekMasa.MasaIsim = masa.MasaIsim;
-                guncellenecekMasa.MasaHesap = masa.MasaHesap;
-                
+                guncellenecekyiyecek.Yiyecekİsim = yiyecek.Yiyecekİsim;
+                guncellenecekyiyecek.Yiyecekİsim = yiyecek.Yiyecekİsim;
+                guncellenecekyiyecek.YiyecekFiyat = yiyecek.YiyecekFiyat;
             }
 
-            db.Masa.Add(masa);
+            db.Yiyecek.Add(yiyecek);
             db.SaveChanges();
             return RedirectToAction("Index");
 
@@ -52,21 +53,22 @@ namespace CafeOtomasyonu.Controllers
 
         public ActionResult Guncelle(int id)
         {
-            var model = db.Masa.Find(id);
+            var model = db.Yiyecek.Find(id);
             if (model == null)
                 return HttpNotFound();
 
-            return View("MasaForm", model);
+            return View("YiyecekForm", model);
         }
 
         public ActionResult Sil(int id)
         {
-            var silinecekMasa = db.Masa.Find(id);
-            if (silinecekMasa == null)
+            var silinecekYiyecek = db.Yiyecek.Find(id);
+            if (silinecekYiyecek == null)
                 return HttpNotFound();
-            db.Masa.Remove(silinecekMasa);
+            db.Yiyecek.Remove(silinecekYiyecek);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+
     }
 }
